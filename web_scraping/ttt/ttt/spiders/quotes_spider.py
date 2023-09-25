9eb298e934176fc595759cff23e87386e7693008
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2023-09-25 15:01:49 +1000

    have some bug of yield

--- web_scraping/ttt/ttt/spiders/quotes_spider.py
+++ web_scraping/ttt/ttt/spiders/quotes_spider.py
@@ -6,13 +6,10 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
-        "https://quotes.toscrape.com/page/1/",
-        "https://quotes.toscrape.com/page/2/",]
+        "https://quotes.toscrape.com/page/1/",]
 
     def parse(self, response):
-        for quote in response.css("div.quote"):
-            yield {
-                "text": quote.css("span.text::text").get(),
-                "author": quote.css("small.author::text").get(),
-                "tags": quote.css("div.tags a.tag::text").getall(),
-            }
+        a = response.xpath("//span[has-class('text')]/text()").getall()
+        yield a
+
+        #may have some problem of yield.
