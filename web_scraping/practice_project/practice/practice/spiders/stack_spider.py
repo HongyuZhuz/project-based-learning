afb3cdb4b312e5269c9b2dcf2037361a6fbbb5e9
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2023-10-09 13:44:57 +1100

    chatGPT yyds

--- web_scraping/practice_project/practice/practice/spiders/stack_spider.py
+++ web_scraping/practice_project/practice/practice/spiders/stack_spider.py
@@ -1,21 +1,20 @@
 import scrapy
-
-
+from ..items import StackItem
 
 class StackSpider(scrapy.Spider):
     name = "stack"
-    #start_urls = ["http://stackoverflow.com/questions?pagesize=50&sort=newest",]
-    start_urls = ["https://realpython.com/web-scraping-with-scrapy-and-mongodb/",]
+    start_urls = ["https://stackoverflow.com/questions?sort=newest",]
 
     def parse(self, response):
-        questions = response.css("h1::text").get()
-        #questions = response.xpath("//div[@class ='s-post-summary--content']").getall()
-        yield questions
-
- #       for question in questions:
- #           item = StackItem()
- #           item['title'] = question.xpath('//*[@class="s-link"]/text()').getall()
- #           yield item
+        
+        questions = response.xpath("//a[@class = 's-link']")
+        
+        for question in questions:
+            item = StackItem()
+            item['title'] = question.xpath("./text()").get()
+            absolute_url = response.urljoin(question.xpath("./@href").get())
+            item['url'] = absolute_url
+            yield item
 
 
     
